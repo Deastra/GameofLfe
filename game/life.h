d1fa594178ae67cda8df04283270fb87fb432feb
Gismat Salimov <gismat.salimov02@gmail.com>
2022-06-08 13:32:42 +0400

    version2(circular) added

--- game/life.h
+++ game/life.h
@@ -11,10 +11,11 @@ int** initiateMap();
 void printMap(int** map);
 int** firstDay();
 int ifAllDead(int** map);
+int countNeighborsCircular(int **map,int x,int y);
 int countNeighbors(int** map,int x, int y);
 int getNextState(int** map,int x,int y,int cf);
 int** nextDay(int** map,int cf);
-void Life();
+void Life(int cf);
 
 
 #endif
--- main.c
+++ main.c
@@ -4,5 +4,5 @@
 
 int main(){
 
-    Life();
+    Life(1);
 }
