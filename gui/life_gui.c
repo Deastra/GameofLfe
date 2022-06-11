d9df9f2fa2238ba486413f63fd50749f0f8764f0
Gismat Salimov <gismat.salimov02@gmail.com>
2022-06-12 00:53:41 +0400

    Refactoring the code, comments added

--- gui/life_gui.c
+++ gui/life_gui.c
@@ -1,13 +1,15 @@
 #include "life_gui.h"
 
+// dimensions of the SDL window
 int height=600;
 int width=600;
-int next=600/15;
+int next=600/15; // size of each cell on the SDL window
 
 
 SDL_Renderer* Create_Window_Renderer(int width, int height){
+    // SDL Window initializing
     SDL_Init(SDL_INIT_VIDEO);
-    SDL_Window * win = SDL_CreateWindow(
+    SDL_Window * win = SDL_CreateWindow( 
         "Game of Life",
         SDL_WINDOWPOS_UNDEFINED,
         SDL_WINDOWPOS_UNDEFINED,
@@ -15,8 +17,9 @@ SDL_Renderer* Create_Window_Renderer(int width, int height){
         height,
         SDL_WINDOW_SHOWN
     );
-    
-    if(win==NULL){printf("%s",SDL_GetError());}
+    // Check window error
+    if(win==NULL){printf("%s",SDL_GetError()); exit(-1);}
+    // Initializing the window renderer
     SDL_Renderer * ren = SDL_CreateRenderer(
     win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
     return ren;
@@ -24,29 +27,31 @@ SDL_Renderer* Create_Window_Renderer(int width, int height){
 }
 
 SDL_Rect SDL_Create_Rect (){
+    // Initializing the SDL rectangle for 
+    // position 0 0 and width and height of the each cell
     SDL_Rect rect={
         .x=0,
         .y = 0,
-        .w = 60,
-        .h = 60
+        .w = next,
+        .h = next
     };
     
     return rect;
 }
 
 void Rect_Update(SDL_Rect* rect,int x, int y){
+    // Position update of rectangle
     (*rect).x=x;
     (*rect).y=y;
 }
 
 void Game_Render(int **map,SDL_Renderer *ren,SDL_Rect *rect){
-    
     for (int i = 0; i < SIZE; i++) {
         for (int j = 0; j < SIZE; j++) {
-            if(map[i][j]==0){SDL_SetRenderDrawColor(ren, 0, 0, 0, 0);}
-            else{SDL_SetRenderDrawColor(ren, 255, 215, 0, 0);}
-            Rect_Update(rect,i*next,j*next);
-            SDL_RenderFillRect(ren, rect);
+            if(map[i][j]==0){SDL_SetRenderDrawColor(ren, 0, 0, 0, 0);}  // Color set for the dead cells to black
+            else{SDL_SetRenderDrawColor(ren, 255, 215, 0, 0);} // Color set for the alive cells to golden
+            Rect_Update(rect,i*next,j*next); // Rectangle position to the next cell
+            SDL_RenderFillRect(ren, rect); // Coloring the current cell on the window 
         }
     }
         
@@ -54,24 +59,26 @@ void Game_Render(int **map,SDL_Renderer *ren,SDL_Rect *rect){
 
 int Game(int version) {
 
-    SDL_Renderer* ren=Create_Window_Renderer(600,600);
+    SDL_Renderer* ren=Create_Window_Renderer(width,height); // Initializing the window and renderer
 
-    SDL_Rect rect=SDL_Create_Rect();
+    SDL_Rect rect=SDL_Create_Rect(); // Initializing the rectangle for coloring the cells
 
-    SDL_Event event;
+    SDL_Event event; // Event handler 
 
     bool running = true;
 
-    int** map=firstDay();
+    int** map=firstDay(); // Getting the map of first day of the life
 
     while (running) {
         
-        SDL_SetRenderDrawColor(ren, 0, 0, 0, 0);
+        SDL_SetRenderDrawColor(ren, 0, 0, 0, 0); // Setting the renderer as a default ( just in case )
         SDL_RenderClear(ren);
-        Game_Render(map,ren,&rect);
-        if ((SDL_PollEvent(&event) && event.type == SDL_QUIT) || ifAllDead(map)) {running = false;}
-        map=nextDay(map,version);
-        sleep(1);
+
+        Game_Render(map,ren,&rect); // Coloring the cells for current day
+        if ((SDL_PollEvent(&event) && event.type == SDL_QUIT) || ifAllDead(map)) {running = false;} // Quit handling
+
+        map=nextDay(map,version); // Get the map of the next day
+        sleep(1); // Slowing down for observing
 
         SDL_RenderPresent(ren);
     }
--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include "life_gui.h"
 
+// Calling the Game Of Life...
+// 0 for the direct, 1 for the circular version
 int main(int argc, char **argv){
-    Game(1);
+    // Let the Life start...
+    Game(1); // version as an argument
     return 0;
 }
